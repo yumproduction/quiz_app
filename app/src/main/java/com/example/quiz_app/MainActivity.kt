2abdd1c14cec4bc04142f370b83e0a2a1c02f6bd
mekhribonu_s <mekhribonu.s@codexoptimus.pro>
2022-06-27 11:19:48 +0500

    view binding

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.quiz_app.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding  //defining the binding class
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        hideSystemBars()
+
+        binding.btnStart.setOnClickListener{
+            print(binding.etName.text.toString().isEmpty())
+            if(binding.etName.text.toString().isEmpty()){
+                Toast.makeText(this, "Please, enetr your name", Toast.LENGTH_SHORT)
+            }
+        }
+    }
+
+    private fun hideSystemBars() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
